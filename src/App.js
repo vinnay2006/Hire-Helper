f50d69f6f26abd54781148f613633e527cec7ec7
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2025-08-03 18:12:30 +0530

    login signup  for user and helper and fetching neraby helpers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+import Tutor from './components/Tutor';
+import Dashboard from './components/Dashboard';
+import UserSignup from './components/UserSignup';
+import HelperSignup from './components/HelperSignup';
+import HelperState from './context/helpers/HelperState';
+import AuthState from './context/helpers/AuthState';
+import About from './components/About';
+import HireHelper from './components/HireHelper';
 
 function App() {
   return (
+    <>
+    <AuthState>
+   <HelperState>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Navbar />
+   <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/tutor" element={<Tutor />} />
+        <Route path="/UserSignup" element={< UserSignup/>} />
+         <Route path="/HelperSignup" element={< HelperSignup/>} />
+         <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/hire" element={<HireHelper />} />
+      </Routes>
     </div>
+   </HelperState>
+   </AuthState>
+    </>
   );
 }
 
