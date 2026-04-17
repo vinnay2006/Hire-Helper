8209fa01e750a6bd81128060495c3ad241702de0
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2026-04-17 13:25:40 +0530

    this deployment level change

--- backend/middleware/fetchhelper.js
+++ backend/middleware/fetchhelper.js
@@ -1,5 +1,5 @@
 var jwt=require('jsonwebtoken');
-const JWT_SECRET="vinayisagooddebator";
+const JWT_SECRET=process.env.jwt_secret;
 const fetchuser=(req,res,next)=>{
 const token=req.header('auth-token');
 if(!token){
