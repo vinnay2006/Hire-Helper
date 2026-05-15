4e224a5e19a6495462f1ae1709244c0ecb6fff5e
vinnay2006 <vinaykumar1973.vkb@gmail.com>
2026-05-15 16:50:02 +0530

    just saved the chnages made in the api url

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -3,14 +3,14 @@ const JWT_SECRET=process.env.jwt_secret;
 const fetchuser=(req,res,next)=>{
 const token=req.header('auth-token');
 if(!token){
-    res.send(401).send({error:"please authenticate using a valid token"});
+   return res.status(401).send({error:"please authenticate using a valid token"});
 }
 try {
  const data =jwt.verify(token,JWT_SECRET);
 req.user=data.user;
 next();   
 } catch (error) {
-  res.status(401).send({error:"please authenticate using a valid token"});   
+ return res.status(401).send({error:"please authenticate using a valid token"});   
 }
 
 }
